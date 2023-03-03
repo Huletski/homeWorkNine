871d905581d4ee55da0efeddcdb533952b0e0627
Huletski Alexander <alexander.huletski@gmail.com>
2023-03-03 17:10:16 +0300

    was saved 6 task

--- Program.cs
+++ Program.cs
@@ -97,6 +97,8 @@ int InputNumbers(string input)
 */
 //Task 5 Написать программу возведения числа А в целую стень B
 
+/*
+
 int raisingToIntegerPower(int a, int b)
 {
     if (b > 0) return a * raisingToIntegerPower(a, b - 1);
@@ -118,3 +120,44 @@ double result = Convert.ToDouble(raisingToIntegerPower(a, b));
 
 if (b < 0) result = Math.Round(1 / result, 3);
 Console.WriteLine($"Результат: {result}");
+
+*/
+
+
+//Task6 Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. Первые два элемента последовательности задаются пользователем
+
+/*
+
+int[] SequenceSum(int a, int b, int n)
+{
+    int[] array = new int[n];
+    array[0] = a;
+    array[1] = b;
+
+    for (int i = 3; i <= array.Length; i++)
+        array[i-1] = array[i-2] + array[i-3];
+    
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+
+        Console.Write($"{array[i]}\t");
+}
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+
+    return int.Parse(Console.ReadLine());
+}
+
+int a = ReadInt("Введите число A:");
+int b = ReadInt("Введите число B:");
+int n = ReadInt("Сколько чисел вывести?");
+
+PrintArray(SequenceSum(a, b, n));
+
+*/
